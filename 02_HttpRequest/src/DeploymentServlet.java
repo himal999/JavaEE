75f0c68d4196d2bf731544be2ca174f13665ad86
himal999 <himal.official99@gmail.com>
2022-04-19 21:09:45 +0530

    h1 tag response genareded

--- 02_HttpRequest/src/DeploymentServlet.java
+++ 02_HttpRequest/src/DeploymentServlet.java
@@ -8,7 +8,7 @@ import java.io.PrintWriter;
 public class DeploymentServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        writer.print("Request res From Deployment Servlet");
+        PrintWriter writer = resp.getWriter(); //meken thma response ek genarate krnne
+        writer.print("<h1>Request res From Deployment Servlet</h1>");
     }
 }
