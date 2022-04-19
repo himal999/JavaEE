f02f0603bd9610738d847cbe351a73a115aba32d
himal999 <himal.official99@gmail.com>
2022-04-19 08:30:32 +0530

    add another servlet

--- 02_HttpRequest/src/MyServlet.java
+++ 02_HttpRequest/src/MyServlet.java
@@ -7,10 +7,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/hello")
-public class HelloServlet extends HttpServlet {
+public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
-        writer.write("Response From Hello servlet");
+        writer.write("Request  rec From MyServlet");
     }
 }
