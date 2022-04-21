698f178bc3736edc843b9bcd90ec4c51aa27df6b
himal999 <himal.official99@gmail.com>
2022-04-21 09:47:02 +0530

    connect db and get data

--- 04_AppOne/src/CustomerServlet.java
+++ 04_AppOne/src/CustomerServlet.java
@@ -4,13 +4,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.*;
+
 
 @WebServlet(urlPatterns = "/customer")
 public class CustomerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        writer.write("Customer");
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con =  DriverManager.getConnection("jdbc:mysql://localhost:3306/JavaEE","root","");
+            PreparedStatement pst = con.prepareStatement("SELECT * FROM `customer`");
+            ResultSet rst = pst.executeQuery();
+            while (rst.next()){
+                System.out.print(rst.getString(1)+" ");
+                System.out.print(rst.getString(2)+" ");
+                System.out.print(rst.getString(3)+" ");
+                System.out.println(rst.getString(4));
+
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
