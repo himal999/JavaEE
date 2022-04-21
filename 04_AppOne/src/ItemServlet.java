4d7e5cfe2c0f7d5d3585819a239622ad360bda94
himal999 <himal.official99@gmail.com>
2022-04-21 17:48:11 +0530

    pass response view

--- 04_AppOne/src/ItemServlet.java
+++ 04_AppOne/src/ItemServlet.java
@@ -3,6 +3,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -16,16 +17,18 @@ public class ItemServlet extends HttpServlet {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/JavaEE", "root", "");
             ResultSet rst = con.prepareStatement("SELECT * FROM `item`").executeQuery();
+            String allItems="";
             while (rst.next()){
-                System.out.println(rst.getString(1));
-                System.out.println(rst.getString(2));
-                System.out.println(rst.getString(3));
-                System.out.println(rst.getString(4));
 
-
-
-
+                String item = "{\"id\":\""+rst.getString(1)+"\",\"name\":\""+rst.getString(2)+"\",\"qty\":"+rst.getString(3)+",\"unitPrice\":"+rst.getString(4)+"},";
+                allItems+=item;
             }
+
+             String json = "["+allItems.substring(0,allItems.length()-1)+"]";
+
+            PrintWriter writer = resp.getWriter();
+            writer.write(json);
+
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
