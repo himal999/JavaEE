56230d5dc0bd68365a4c56c16662277f71ef3592
himal999 <himal.official99@gmail.com>
2022-04-27 15:50:07 +0530

    PUT metnof using update customer

--- 05_AppTwo/src/CustomerServlet.java
+++ 05_AppTwo/src/CustomerServlet.java
@@ -71,4 +71,61 @@ public class CustomerServlet extends HttpServlet {
             e.printStackTrace();
         }
     }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        try {
+            String customerId = req.getParameter("customerId");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/JavaEE","root","");
+            PreparedStatement pst = con.prepareStatement("DELETE FROM `customer` WHERE id=?");
+            pst.setObject(1,customerId);
+            PrintWriter writer = resp.getWriter();
+            if(pst.executeUpdate()>0){
+                writer.write("Customer Delete Success");
+            }else{
+                writer.write("Try Again");
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String customerId = req.getParameter("customerId");
+        String customerName = req.getParameter("customerName");
+        String customerAddress = req.getParameter("customerAddress");
+        String customerSalary = req.getParameter("customerSalary");
+        System.out.println(customerId+" "+customerName+" "+customerAddress+" "+customerSalary);
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/JavaEE","root","");
+            PreparedStatement pst = con.prepareStatement("UPDATE `customer` SET  `name`=? ,address=? ,salary=? WHERE id=?");
+
+            pst.setObject(1,customerName);
+            pst.setObject(2,customerAddress);
+            pst.setObject(3,customerSalary);
+            pst.setObject(4,customerId);
+
+
+
+            PrintWriter writer = resp.getWriter();
+            if(pst.executeUpdate()>0){
+                writer.write("Customer Update Success");
+                return;
+            }else{
+                writer.write("Try Again");
+                return;
+            }
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
